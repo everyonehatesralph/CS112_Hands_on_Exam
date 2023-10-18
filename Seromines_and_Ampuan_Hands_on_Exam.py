3d064b43ae110d701793096a5daaa95192e0dfa5
Ralph Joshua <115396399+everyonehatesralph@users.noreply.github.com>
2023-10-18 04:29:04 -0700

    Update Seromines_and_Ampuan_Hands_on_Exam.py

--- Seromines_and_Ampuan_Hands_on_Exam.py
+++ Seromines_and_Ampuan_Hands_on_Exam.py
@@ -3,7 +3,7 @@ name2 = "Ayyah Ampuan"
 print("By:", name1,"and", name2)
 
 decimal_number = eval(input("Please enter a decimal number: "))
-print("[1] Decimal number to Binary number\n[2] Decimal number to Octal\n[3] Decimal number to Hexadecimal")
+print("[1] Decimal number to Binary number\n[2] Decimal number to Octal\n[3] Decimal number to Hexadecimal\n")
 the_conversion_choice = int(input("Select 1,2 or 3:"))
 
 
@@ -17,9 +17,6 @@ elif the_conversion_choice == 3:
     result = format(decimal_number, 'x')
     print("If the Decimal", decimal_number, "Converted to Hexadecimal Number is", result)
 else:
-    result = "Bugok ka?!!! 1 ,2 og 3 raman sa selection. BOBO!!, Undang Skwela uii uli dadto!!"
+    result = "Invalid Result.\nPlease Try again and Follow the given selection number!"
     print(result)
-
-
-
 
